a9f7ea021006fcea1b0f6f2ffc6e97faed5574ae
DarshanAware <144710362+blaze-d83@users.noreply.github.com>
2024-09-06 16:43:25 +0530

    undotree works

--- lua/plugins/editor/undotree.lua
+++ lua/plugins/editor/undotree.lua
@@ -1,6 +1,7 @@
 return {
 	"mbbill/undotree",
 	config = function()
+		vim.g.undotree_DiffCommand = "C:/Program Files/Git/git-bash.exe"
 		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 	end,
 }
