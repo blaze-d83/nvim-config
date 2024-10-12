833ee7bae3a62ccb1133ca858ccb8c46088ea6b7
DarshanAware <144710362+blaze-d83@users.noreply.github.com>
2024-10-12 14:43:51 +0530

    Reduce startup time by lazy loading

--- lua/plugins/nav/telescope.lua
+++ lua/plugins/nav/telescope.lua
@@ -1,25 +1,72 @@
 return { -- Fuzzy Finder
 	"nvim-telescope/telescope.nvim",
-	event = "VimEnter",
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
 			"nvim-telescope/telescope-fzf-native.nvim",
-
-			-- This is only run then, not every time Neovim starts up.
-			build = "make",
-
-			-- installed and loaded.
+			build = "make", -- This is only run then, not every time Neovim starts up.
 			cond = function()
 				return vim.fn.executable("make") == 1
 			end,
 		},
 		{ "nvim-telescope/telescope-ui-select.nvim" },
-
 		-- Useful for getting pretty icons, but requires a Nerd Font.
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
+	keys = {
+		{ "<leader>sh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", desc = "[S]earch [H]elp" },
+		{ "<leader>sk", "<cmd>lua require('telescope.builtin').keymaps()<CR>", desc = "[S]earch [K]eymaps" },
+		{ "<leader>sf", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "[S]earch [F]iles" },
+		{ "<leader>ss", "<cmd>lua require('telescope.builtin').builtin()<CR>", desc = "[S]earch [S]elect Telescope" },
+		{ "<leader>st", "<cmd>lua require('telescope.builtin').git_files()<CR>", desc = "[S]earch [G]it Files" },
+		{ "<leader>slr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>", desc = "[] Lsp References" },
+		{ "<leader>sld", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>", desc = "[] Lsp Definitions" },
+		{ "<leader>sw", "<cmd>lua require('telescope.builtin').grep_string()<CR>", desc = "[S]earch current [W]ord" },
+		{ "<leader>sg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "[S]earch by [G]rep" },
+		{ "<leader>sd", "<cmd>lua require('telescope.builtin').diagnostics()<CR>", desc = "[S]earch [D]iagnostics" },
+		{ "<leader>sr", "<cmd>lua require('telescope.builtin').resume()<CR>", desc = "[S]earch [R]esume" },
+		{
+			"<leader>s.",
+			"<cmd>lua require('telescope.builtin').oldfiles()<CR>",
+			desc = '[S]earch Recent Files ("." for repeat)',
+		},
+		{
+			"<leader><leader>",
+			"<cmd>lua require('telescope.builtin').buffers()<CR>",
+			desc = "[ ] Find existing buffers",
+		},
+		{ "<leader>sq", "<cmd>lua require('telescope.builtin').quickfix()<CR>", desc = "[] Quick Fix" },
+		{
+			"<leader>/",
+			function()
+				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+				require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end,
+			{ desc = "[/] Fuzzily search in current buffer" },
+		},
+		{
+			"<leader>s/",
+			function()
+				require("telescope.builtin").live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end,
+			{ desc = "[S]earch [/] in Open Files" },
+		},
+		{
+			"<leader>sn",
+			function()
+				require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+			end,
+			{ desc = "[S]earch [N]eovim files" },
+		},
+	},
+	cmd = { "Telescope" }, -- This will load telescope when you use the :Telescope command
 	config = function()
 		require("telescope").setup({
 			defaults = {
@@ -51,43 +98,5 @@ return { -- Fuzzy Finder
 		-- Enable Telescope extensions if they are installed
 		pcall(require("telescope").load_extension, "fzf")
 		pcall(require("telescope").load_extension, "ui-select")
-
-		-- See `:help telescope.builtin`
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-		vim.keymap.set("n", "<leader>st", builtin.git_files, { desc = "[S]earch [G]it Files" })
-		vim.keymap.set("n", "<leader>slr", builtin.lsp_references, { desc = "[] Lsp References" })
-		vim.keymap.set("n", "<leader>sld", builtin.lsp_definitions, { desc = "[] Lsp Definitions" })
-		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-		vim.keymap.set("n", "<leader>sq", builtin.quickfix, { desc = "[] Quick Fix" })
-
-		-- Slightly advanced example of overriding default behavior and theme
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
-			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, { desc = "[/] Fuzzily search in current buffer" })
-
-		vim.keymap.set("n", "<leader>s/", function()
-			builtin.live_grep({
-				grep_open_files = true,
-				prompt_title = "Live Grep in Open Files",
-			})
-		end, { desc = "[S]earch [/] in Open Files" })
-
-		-- Shortcut for searching your Neovim configuration files
-		vim.keymap.set("n", "<leader>sn", function()
-			builtin.find_files({ cwd = vim.fn.stdpath("config") })
-		end, { desc = "[S]earch [N]eovim files" })
 	end,
 }
--- lua/plugins/utils/debugger.lua
+++ lua/plugins/utils/debugger.lua
@@ -1,95 +1,174 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
-        "nvim-telescope/telescope-dap.nvim",
-        "theHamsta/nvim-dap-virtual-text",
-        "nvim-neotest/nvim-nio",
-        "leoluz/nvim-dap-go",
-    },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
-        local dap_virtual_text = require("nvim-dap-virtual-text")
-        local dap_go = require("dap-go")
-        local telescope = require('telescope')
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-telescope/telescope-dap.nvim",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-neotest/nvim-nio",
+		"leoluz/nvim-dap-go",
+	},
+	-- Lazy load the plugin only when one of these commands or keybindings is triggered
+	keys = {
+		{
+			"<leader>db",
+			function()
+				require("dap").toggle_breakpoint()
+			end,
+			desc = "Toggle Breakpoint",
+		},
+		{
+			"<leader>dc",
+			function()
+				require("dap").continue()
+			end,
+			desc = "Continue",
+		},
+		{
+			"<leader>dso",
+			function()
+				require("dap").step_over()
+			end,
+			desc = "Step Over",
+		},
+		{
+			"<leader>dsi",
+			function()
+				require("dap").step_into()
+			end,
+			desc = "Step Into",
+		},
+		{
+			"<leader>dsx",
+			function()
+				require("dap").step_out()
+			end,
+			desc = "Step Out",
+		},
+		{
+			"<leader>dr",
+			function()
+				require("dap").repl.open()
+			end,
+			desc = "Open REPL",
+		},
+		{
+			"<leader>dl",
+			function()
+				require("dap").run_last()
+			end,
+			desc = "Run Last",
+		},
+		{
+			"<leader>dt",
+			function()
+				require("dap-go").debug_test()
+			end,
+			desc = "Debug Test",
+		},
+		{
+			"<leader>dh",
+			mode = { "n", "v" },
+			function()
+				require("dap.ui.widgets").hover()
+			end,
+			desc = "Hover",
+		},
+		{
+			"<leader>dp",
+			mode = { "n", "v" },
+			function()
+				require("dap.ui.widgets").preview()
+			end,
+			desc = "Preview",
+		},
+		{
+			"<leader>df",
+			function()
+				require("dap.ui.widgets").centered_float(require("dap.ui.widgets").frames)
+			end,
+			desc = "Frames",
+		},
+		{
+			"<leader>dw",
+			function()
+				require("dap.ui.widgets").centered_float(require("dap.ui.widgets").scopes)
+			end,
+			desc = "Scopes",
+		},
+	},
+	cmd = {
+		"DapToggleBreakpoint",
+		"DapContinue",
+		"DapStepOver",
+		"DapStepInto",
+		"DapStepOut",
+		"DapOpenRepl",
+		"DapRunLast",
+		"DapUIOpen",
+		"DapUIClose",
+		"DapToggleUI",
+	},
+	config = function()
+		-- This part is run only when the plugin is loaded (lazy-loaded)
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
+		local dap_go = require("dap-go")
+		local telescope = require("telescope")
 
-        dapui.setup()
-        dap_virtual_text.setup({
-            enabled = true,                      -- enable this plugin (the default)
-            enabled_commands = true,             -- create commands DapVirtualTextEnable, DapVirtualTextDisable, and DapVirtualTextToggle (the default)
-            highlight_changed_variables = true,  -- highlight changed values with NvimDapVirtualTextChanged (the default)
-            highlight_new_as_changed = false,    -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
-            show_stop_reason = true,             -- show stop reason when stopped for exceptions
-            commented = false,                   -- prefix virtual text with comment string
-            only_first_definition = true,        -- only show virtual text at first definition (if there are multiple)
-            all_references = false,              -- show virtual text on all references of the variable (not only definitions)
-            filter_references_pattern = '<module>',  -- pattern for filtering references (modname/functionname is shown)
-        })
-        telescope.load_extension('dap')
-        dap_go.setup()
+		dapui.setup()
+		dap_virtual_text.setup({
+			enabled = true,
+			enabled_commands = true,
+			highlight_changed_variables = true,
+			highlight_new_as_changed = false,
+			show_stop_reason = true,
+			commented = false,
+			only_first_definition = true,
+			all_references = false,
+			filter_references_pattern = "<module>",
+		})
+		telescope.load_extension("dap")
+		dap_go.setup()
 
-        vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "" })
+		-- Define DAP signs (breakpoint icons)
+		vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "" })
 
-        -- Setup DAP for LLDB (Rust)
-        dap.adapters.lldb = {
-            type = "executable",
-            command = "C:\\mingw64\\bin\\lldb-vscode.exe", -- Ensure this path is correct
-            name = "lldb",
-        }
+		-- Configure DAP for LLDB (Rust)
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "C:\\mingw64\\bin\\lldb-vscode.exe", -- Ensure this path is correct
+			name = "lldb",
+		}
 
-        dap.configurations.rust = {
-            {
-                name = "Launch",
-                type = "lldb",
-                request = "launch",
-                program = function()
-                    return vim.fn.input({
-                        prompt = "Path to executable: ",
-                        default = vim.fn.getcwd() .. "\\target\\debug\\",
-                        completion = "file"
-                    })
-                end,
-                cwd = "${workspaceFolder}",
-                stopOnEntry = false,
-                args = {},
-                runInTerminal = false,
-            },
-        }
+		dap.configurations.rust = {
+			{
+				name = "Launch",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input({
+						prompt = "Path to executable: ",
+						default = vim.fn.getcwd() .. "\\target\\debug\\",
+						completion = "file",
+					})
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+				runInTerminal = false,
+			},
+		}
 
-        -- Open DAP UI
-        dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-        end
-
-        -- DAP keymaps
-        local keymap = vim.keymap.set
-        local opts = { desc = "", noremap = true, silent = true }
-
-        keymap("n", "<leader>db", function() dap.toggle_breakpoint() end, vim.tbl_extend("keep", opts, { desc = "Toggle Breakpoint" }))
-        keymap("n", "<leader>dc", function() dap.continue() end, vim.tbl_extend("keep", opts, { desc = "Continue" }))
-        keymap("n", "<leader>dso", function() dap.step_over() end, vim.tbl_extend("keep", opts, { desc = "Step Over" }))
-        keymap("n", "<leader>dsi", function() dap.step_into() end, vim.tbl_extend("keep", opts, { desc = "Step Into" }))
-        keymap("n", "<leader>dsx", function() dap.step_out() end, vim.tbl_extend("keep", opts, { desc = "Step Out" }))
-        keymap("n", "<leader>dr", function() dap.repl.open() end, vim.tbl_extend("keep", opts, { desc = "Open REPL" }))
-        keymap("n", "<leader>dl", function() dap.run_last() end, vim.tbl_extend("keep", opts, { desc = "Run Last" }))
-        keymap("n", "<leader>dt", function() dap_go.debug_test() end, vim.tbl_extend("keep", opts, { desc = "Debug Test" }))
-        keymap({ "n", "v" }, "<leader>dh", function() require("dap.ui.widgets").hover() end, vim.tbl_extend("keep", opts, { desc = "Hover" }))
-        keymap({ "n", "v" }, "<leader>dp", function() require("dap.ui.widgets").preview() end, vim.tbl_extend("keep", opts, { desc = "Preview" }))
-        keymap("n", "<leader>df", function()
-            local widgets = require("dap.ui.widgets")
-            widgets.centered_float(widgets.frames)
-        end, vim.tbl_extend("keep", opts, { desc = "Frames" }))
-        keymap("n", "<leader>dw", function()
-            local widgets = require("dap.ui.widgets")
-            widgets.centered_float(widgets.scopes)
-        end, vim.tbl_extend("keep", opts, { desc = "Scopes" }))
-    end,
+		-- Open and close DAP UI automatically
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+	end,
 }
-
