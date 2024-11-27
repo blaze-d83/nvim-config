cf760f58a7b7e70117bbe151286e8bfc588a0252
blaze-d83 <d.awareai83@gmail.com>
2024-11-27 11:07:46 +0000

    kanagawa theme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -133,7 +133,7 @@ local themes = {
 }
 
 -- Define the variable to hold the selected theme
-local selected_theme = "rose-pine"
+local selected_theme = "kanagawa"
 
 -- If no theme is selected (i.e., `selected_theme` is empty or "default"), use the default colorscheme
 if selected_theme == "" or selected_theme == "default" then
